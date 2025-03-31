d426c25d247e1ff0066a0e1cecc7e5c68ecb253e
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-03-31 11:54:10 -0500

    Agregar adaptador de Vercel y actualizar dependencias en package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   output: "server",
+  adapter: vercel(),
 });
