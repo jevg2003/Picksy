d0c405500709f8463c6d80e7fec41a0a4f840268
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-04-05 19:27:55 -0500

    Agregar nuevas interfaces de tipo para imágenes y actualizar la interfaz de producto con nuevos campos

--- src/types/api/product.ts
+++ src/types/api/product.ts
@@ -1,14 +1,19 @@
 import type { Category } from "./category";
+import type { Image } from "./image";
 
 export interface Product {
   id: number;
   name: string;
-  image: string;
   price: number;
   description: string;
   stock: number;
-  isBlock: boolean;
+  discount: number;
+  characteristics: string | null;
+  enabled: boolean;
+  createdAt: string;
+  updatedAt: string;
   category: Category;
+  images: Image[];
 }
 
 export interface ProductResponse {
