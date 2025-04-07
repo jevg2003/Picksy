a13efeb73d1a19cc074b648f0c753d6147470f3e
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-04-07 12:41:22 -0500

    Actualizar manejo de errores en queryApi y agregar funciones para obtener, crear, actualizar y eliminar productos.

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,10 +2,11 @@ export const API_BASE_URL = "https://picksy-api.vercel.app/api/v1";
 
 export const queryApi = async (url: string, options: RequestInit) => {
   const response = await fetch(`${API_BASE_URL}${url}`, options);
+  const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(`${response.status} Error: ${response.statusText}`);
+    throw new Error(`${data.status} Error: ${data.message}`);
   }
 
-  return response.json();
-}
+  return data;
+};
